e9a6a3ee92912b9e7740db57ab07c1a2bc4f2e6f
Einstainer <rana2_15@uni.worc.ac.uk>
2017-10-23 14:36:48 +0100

    Changing base code
    Removing the useless code and adding in the worksheet test

---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+		
+	
+
+
+// device APIs are available
+// Runs after the HTML file has been read fully
+    function onDeviceReady() {   
+	alert("device ready");
+    }
+
